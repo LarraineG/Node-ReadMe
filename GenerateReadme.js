be127ccf2be74168c275af8f3a656c75b19215d8
Larraine Gonzales <larrainegonzales@hotmail.com>
2020-08-26 09:06:17 -0500

    Added bulk of code, currently has syntax error

---
+++ GenerateReadme.js
@@ -0,0 +1,55 @@
+// function to generate markdown for README
+//ES6 temporal literals uses backticks
+function generateMarkdown(data) {
+    return `# ${data.title}
+    ## ${data.Username}
+    ## ${data.Email}
+    ## ${data.Name}
+    ## ${data.Description}
+    ## ${data.License}
+    ## ${data.Installation}
+    ## ${data.Tests}
+    ## ${data.Usage}
+    ## ${data.Contribution}
+    ## Table of Contents
+    ## Description
+    * [Description](#Description)
+    ## Installation
+    * [Installation](#Installation)
+    ## License
+    * [License](#License)
+    ## Usage
+    * [Usage](#Usage)
+    ## Contribution
+    * [Contribution](#Contribution)
+    ## Tests
+    * [Tests](#Tests)
+  `;
+  }
+  module.exports = generateMarkdown;
+  
+  
+  
+  
+  
+
+//   generate markdown file
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+//   Message Angela
+  
+  
